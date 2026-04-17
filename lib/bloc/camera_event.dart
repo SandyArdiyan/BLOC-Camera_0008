36c32a419b2013251138bc043302d641d57dee06
SandyArdiyan <sanardiyan881@gmail.com>
2026-04-17 09:57:41 +0700

    Mengatur size camera

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -11,6 +11,11 @@ final class TakePicture extends CameraEvent {
   final void Function(File imageFile) onPictureTaken;
   TakePicture(this.onPictureTaken);
 }
+final class TopFoucus extends CameraEvent {\
+  final Offset position;
+  final Size previewSize;
+  TopFoucus(this.position, this.previewSize);
+}
 
 
 
